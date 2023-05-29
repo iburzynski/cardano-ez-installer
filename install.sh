6eba48909d2a927d33adf793487f9305896072b1
Ian Burzynski <23251244+iburzynski@users.noreply.github.com>
2023-05-29 15:59:03 -0400

    Refactor install.sh, use existing Python if sufficient

--- install.sh
+++ install.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/env bash
 
-# Runs Python installer in a temporary Nix shell:
-nix-shell -p python311 --run "
+run_python_script() {
   source .env
   if ! python3 main.py; then
     exit 1
   fi
-"
+}
 
-if [ $? -eq 0 ]; then
-  read -p "Clean up nix-store? [Y/n]" answer
+cleanup_nix_store() {
+  read -p "Clean up nix-store? [Y/n] " answer
   answer=${answer:-Y}
 
-  if [[ $answer =~ ^[Yy]$ ]]
-  then
+  if [[ $answer =~ ^[Yy]$ ]]; then
     nix-collect-garbage -d
   fi
+}
+
+# Check if python3 command is available and Python version is 3.6 or greater
+if command -v python3 &> /dev/null &&
+   python_version=$(python3 -c "import sys; print(sys.version_info.major * 10 + sys.version_info.minor)") &&
+   [[ $python_version -ge 36 ]]; then
+  run_python_script
+  cleanup_nix_store
+  else
+    # If not, use nix-shell with Python 3.11
+    nix-shell -p python311 --run "$(declare -f run_python_script); run_python_script"
+
+    if [ $? -eq 0 ]; then
+      cleanup_nix_store
+    fi
 fi
