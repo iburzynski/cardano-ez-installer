a392274d78d61f91640aca450c473a661c5cdcad
Ian Burzynski <23251244+iburzynski@users.noreply.github.com>
2023-08-21 16:06:39 -0400

    Fix line breaks in daemon_snippet_lines

--- src/dotfiles.py
+++ src/dotfiles.py
@@ -8,11 +8,10 @@ from .utils import ind, print_fail, print_neutral, print_success
 
 # Nix daemon failsafe for MacOS users
 daemon_path = "'/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'"
-daemon_snippet_lines: list[str] = f"""
-# Nix
-[ -e {daemon_path} ] && . {daemon_path}
-# End Nix\n
-""".splitlines()
+daemon_snippet_lines = [
+    "# Nix",
+    f"[ -e {daemon_path} ] && . {daemon_path}",
+    "# End Nix\n"]
 
 
 def overwrite_dotfile_safely(dotfile_path: str, new_content: list[str]):
